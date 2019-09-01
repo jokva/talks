156a3014b0bdd4e67985def91de7b35e511092b4
Jørgen Kvalsvik <jokva@equinor.com>
2019-09-01 15:19:01 +0200

    v23

--- building-python/my-package/custom-target.cmake
+++ building-python/my-package/custom-target.cmake
@@ -1,7 +1,5 @@
 add_custom_target(
     my-package ALL
-    SOURCES ${setup.py}
-    DEPENDS ${setup.py}
     VERBATIM
     WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
     COMMAND ${python} ${setup.py}
@@ -9,11 +7,9 @@ add_custom_target(
         build # setup.py build args
             --cmake-executable ${CMAKE_COMMAND}
             --generator ${CMAKE_GENERATOR}
-            ${PYTHON_BUILD_TYPE}
         -- # scikit-build cmake args
             -Dcore_DIR=${LIB_BINARY_DIR}
             -DCMAKE_INSTALL_RPATH_USE_LINK_PATH=ON
             -DCMAKE_INSTALL_RPATH=$<TARGET_FILE_DIR:core>
             -DCMAKE_INSTALL_NAME_DIR=$<TARGET_FILE_DIR:core>
 )
-add_dependencies(my-package core)
