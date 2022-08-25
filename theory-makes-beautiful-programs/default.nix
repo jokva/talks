9a4839cc9e357dfdc91169fae91d3e2b356b8290
Jørgen Kvalsvik <jorgen.kvalsvik@woven-planet.global>
2022-08-25 14:45:32 +0200

    Add 'Theory makes beautiful programs'
    For NDC Techtown 2022

---
+++ theory-makes-beautiful-programs/default.nix
@@ -0,0 +1,35 @@
+with import <nixpkgs> {};
+
+stdenv.mkDerivation {
+  name = "talk-techtown-2022";
+  buildInputs = [(texlive.combine {
+                    inherit (texlive)
+                      scheme-small
+
+                      # Add other LaTeX libraries (packages) here as needed, e.g:
+                      # stmaryrd amsmath pgf
+                      algorithms
+                      algorithmicx
+                      beamer
+                      listings
+                      hyperref
+                      xcolor
+                      braket
+                      amsmath
+                      pgf # tikz
+
+                      # build tools
+                      latexmk
+                      ;
+                  })
+                  graphviz
+                  dot2tex
+                ];
+  src = ./.;
+  buildPhase = "make";
+
+  meta = with lib; {
+    description = "Slides for techtown 2022";
+    license = licenses.bsd3;
+  };
+}
